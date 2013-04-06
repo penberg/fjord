7aaedba94787ec21c8142ae2f5f184650014cb4e
alexbiehl <alex.biehl@gmail.com>
2013-04-06 11:32:05 +0200

    some ast classes

---
+++ src/main/java/fjord/ast/expr/ApplicationExpression.java
@@ -0,0 +1,5 @@
+package fjord.ast.expr;
+
+public class ApplicationExpression {
+
+}
---
+++ src/main/java/fjord/ast/expr/Expression.java
@@ -0,0 +1,7 @@
+package fjord.ast.expr;
+
+import fjord.ast.Node;
+
+public interface Expression extends Node {
+
+}
