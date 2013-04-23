017bf8e44683718ed86faa1c2d639115c8932cda
alexbiehl <alex.biehl@gmail.com>
2013-04-23 18:54:42 +0200

    Parser/AST: NodeWithChildren class

---
+++ src/main/java/fjord/ast/NodeWithChildren.java
@@ -0,0 +1,22 @@
+package fjord.ast;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public abstract class NodeWithChildren<T extends Node> implements Node {
+  
+  protected final List<T> children;
+  
+  public NodeWithChildren() {
+    this(new ArrayList<T>());
+  }
+  
+  public NodeWithChildren(List<T> children) {
+    this.children = children;
+  }
+  
+  public void addChild(T child) {
+    children.add(child);
+  }
+  
+} 
