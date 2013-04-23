0bd45452e4684af8e9765c6cc11cdf14b49a2969
alexbiehl <alex.biehl@gmail.com>
2013-04-23 20:07:22 +0200

    Parser/AST: cons pattern

---
+++ src/main/java/fjord/ast/pat/ConsPattern.java
@@ -0,0 +1,27 @@
+package fjord.ast.pat;
+
+import fjord.ast.NodeVisitor;
+
+public class ConsPattern implements Pat {
+
+  private final Pat left;
+  
+  private final Pat right;
+  
+  public ConsPattern(Pat left, Pat right) {
+    this.left = left;
+    this.right = right;
+  }
+  
+  @Override
+  public void accept(NodeVisitor visitor) {}
+
+  public Pat getLeft() {
+    return left;
+  }
+  
+  public Pat getRight() {
+    return right;
+  }
+
+}
