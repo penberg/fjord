eecc8922e596dd0ff6f6509506d41cc47aadd498
alexbiehl <alex.biehl@gmail.com>
2013-04-24 13:48:28 +0200

    Parser/AST: union type defn

---
+++ src/main/java/fjord/ast/typedefn/UnionTypeDefn.java
@@ -0,0 +1,40 @@
+package fjord.ast.typedefn;
+
+import java.util.Collections;
+import java.util.List;
+
+import fjord.ast.Node;
+import fjord.ast.NodeVisitor;
+
+public class UnionTypeDefn implements Node {
+
+  private final TypeName typeName;
+  
+  private final List<UnionTypeCase> unionTypeCases;
+  
+  private final List<TypeDefnElement> typeDefnElements;
+  
+  public UnionTypeDefn(TypeName typeName, List<UnionTypeCase> unionTypeCases, List<TypeDefnElement> typeDefnElements) {
+    this.typeName = typeName;
+    this.unionTypeCases = unionTypeCases;
+    this.typeDefnElements = typeDefnElements != null ? typeDefnElements : Collections.<TypeDefnElement>emptyList();
+  }
+  
+  @Override
+  public void accept(NodeVisitor visitor) {
+
+  }
+
+  public TypeName getTypeName() {
+    return typeName;
+  }
+
+  public List<UnionTypeCase> getUnionTypeCases() {
+    return unionTypeCases;
+  }
+
+  public List<TypeDefnElement> getTypeDefnElements() {
+    return typeDefnElements;
+  }
+
+}
